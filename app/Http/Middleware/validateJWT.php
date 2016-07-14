50af6d3864261823bb1e5c2a468d456405bc93d3
Marcelo Rafael <marcelo@make.pro.br>
2016-07-14 16:21:42 -0300

    Models

--- app/Http/Middleware/validateJWT.php
+++ app/Http/Middleware/validateJWT.php
@@ -16,22 +16,50 @@ class validateJWT
 
     public static function getToken() {
         $headers = getallheaders();
-        $auth = $headers['Authorization'];
+        $auth = isset($headers['Authorization']) ? $headers['Authorization'] : '';
 
-        if(preg_match('/\bBearer\b/i', $auth)) {
-            $auth = preg_replace('/\bBearer\b/i','', $auth);
-            return $auth;
-        } else {
-            return false;
+        if($auth) {
+            if (preg_match('/\bBearer\b/i', $auth)) {
+                $auth = trim(preg_replace('/\bBearer\b/i', '', $auth));
+                return $auth;
+            }
         }
         return false;
     }
 
+    public static function JWTStructureVerify() {
+        if($token = self::getToken()) {
+            $t = explode('.', $token);
+            $header = $t[0];
+            $payload = $t[1];
+            $assignature = $t[2];
 
+            if($header) {
+                $header = json_encode(base64_decode($header));
+            }
+            if($payload) {
+                $payload = json_encode(base64_decode($payload));
+            }
+            if($assignature) {
+                $assignature = json_encode(base64_decode($assignature));
+
+            }
+        }
+        return false;
+    }
+
+    public static function JWTExpired() {
+        if(self::getToken()) {
+
+        } else {
+            return true;
+        }
+    }
 
     public static function isValid() {
-        if(self::JWTExpired()) {
-            return false;
+        echo self::JWTStructureVerify();exit;
+        if(!self::JWTExpired()) {
+            return true;
         }
     }
 
@@ -39,11 +67,11 @@ class validateJWT
     public function handle($request, Closure $next)
     {
         self::isValid();exit;
-        if(self::isValid()) {
+        /*if(self::isValid()) {
 
         } else {
             return response('Unauthorized', 401);
-        }
+        }*/
         return $next($request);
     }
 }
